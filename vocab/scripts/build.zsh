3d860f797e562a03c0d41073d77d3ee5153bb303
Ronald Koenig <ronald.koenig@spherity.com>
2026-02-04 00:13:16 +0100

    add _redirects

--- vocab/scripts/build.zsh
+++ vocab/scripts/build.zsh
@@ -4,3 +4,5 @@ npx yml2vocab -c
 
 cp ../../../src/class-diagram.mermaid class-diagram.mermaid
 npx -p @mermaid-js/mermaid-cli mmdc -i class-diagram.mermaid -o class-diagram.svg
+
+cp ../../../src/_redirects ../../_redirects
