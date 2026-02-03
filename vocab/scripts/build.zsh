9e5ef2a687363c297c12894c71ec77f9fa0485bb
Ronald Koenig <ronald.koenig@spherity.com>
2026-02-04 00:06:57 +0100

    add vocab

---
+++ vocab/scripts/build.zsh
@@ -0,0 +1,6 @@
+cp ../../../src/template.html template.html
+cp ../../../src/vocabulary.yml vocabulary.yml
+npx yml2vocab -c
+
+cp ../../../src/class-diagram.mermaid class-diagram.mermaid
+npx -p @mermaid-js/mermaid-cli mmdc -i class-diagram.mermaid -o class-diagram.svg
