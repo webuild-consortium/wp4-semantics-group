40cd11b5d8a866fb551d074032cfb99591a01a7e
Ronald Koenig <ronald.koenig@spherity.com>
2026-02-26 19:18:02 +0100

    feat: publish github pages

--- vocab/scripts/build.zsh
+++ vocab/scripts/build.zsh
@@ -1,8 +1,12 @@
-cp ../../../src/template.html template.html
-cp ../../../src/vocabulary.yml vocabulary.yml
-npx yml2vocab -c
+#!/usr/bin/env zsh
+VOCAB='docs/ebw/v0.1'
+CONTEXT='docs/ebw-context/v0.1'
+echo Generating static page $VOCAB
+npx yml2vocab -v vocab/src/vocabulary.yml -t vocab/src/template.html -c
+mv vocab/src/vocabulary.html $VOCAB
+mv vocab/src/vocabulary.jsonld $VOCAB
+mv vocab/src/vocabulary.ttl $VOCAB
+mv vocab/src/vocabulary.context.jsonld $CONTEXT
+sed -i.bak '/^# Context files and their mentions/,$d' $VOCAB/vocabulary.ttl
 
-cp ../../../src/class-diagram.mermaid class-diagram.mermaid
-npx -p @mermaid-js/mermaid-cli mmdc -i class-diagram.mermaid -o class-diagram.svg
-
-cp ../../../src/_redirects ../../_redirects
+npx -p @mermaid-js/mermaid-cli mmdc -i vocab/src/class-diagram.mermaid -o $VOCAB/class-diagram.svg
