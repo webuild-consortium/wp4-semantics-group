de2cb237e5eaf96ec99208a7dbf26a1acb1b597c
Ronald Koenig <ronald.koenig@spherity.com>
2026-03-06 19:20:57 +0100

    feat: updated folder structure, build.zsh and README to fit w3id.org perma link

--- vocab/scripts/build.zsh
+++ vocab/scripts/build.zsh
@@ -1,12 +1,16 @@
 #!/usr/bin/env zsh
-VOCAB='docs/ebw/v0.1'
-CONTEXT='docs/ebw-context/v0.1'
-echo Generating static page $VOCAB
+VOCAB='docs/ebwv'
+VERSION='v0.1'
+# generate vocab from yaml
+echo Generating static page vocabulary
 npx yml2vocab -v vocab/src/vocabulary.yml -t vocab/src/template.html -c
 mv vocab/src/vocabulary.html $VOCAB
 mv vocab/src/vocabulary.jsonld $VOCAB
 mv vocab/src/vocabulary.ttl $VOCAB
-mv vocab/src/vocabulary.context.jsonld $CONTEXT
+mv vocab/src/vocabulary.context.jsonld $VOCAB/$VERSION
 sed -i.bak '/^# Context files and their mentions/,$d' $VOCAB/vocabulary.ttl
-
 npx -p @mermaid-js/mermaid-cli mmdc -i vocab/src/class-diagram.mermaid -o $VOCAB/class-diagram.svg
+# retrieve terminology
+curl 'https://sanastot.suomi.fi/api/getTerminologyAsFile?prefix=webuild&fileType=JSON-LDfilename=WE%20BUILD%20Terminology' > $VOCAB/terminology.jsonld
+curl 'https://sanastot.suomi.fi/api/getTerminologyAsFile?prefix=webuild&fileType=Turtle&filename=WE%20BUILD%20Terminology' > $VOCAB/terminology.ttl
+
